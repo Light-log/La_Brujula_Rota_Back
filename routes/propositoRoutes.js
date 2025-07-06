ecf79b2986d9f9c4dbc55ac4d2c649c9988b1d84
David <david.isturiz.2611@gmail.com>
2025-07-06 15:48:38 -0400

    resol

--- routes/propositoRoutes.js
+++ routes/propositoRoutes.js
@@ -1,8 +1,9 @@
+// routes/propositoRoutes.js
+
 const express = require('express');
 const router = express.Router();
-const { crearProposito, obtenerPropositos } = require('../controllers/propositoController');
+const { crearProposito } = require('../controllers/propositoController');
 
 router.post('/', crearProposito);
-router.get('/', obtenerPropositos);
 
 module.exports = router;
